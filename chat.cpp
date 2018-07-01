d72f296edf68d1061c204d52faabd6b0c3a52fe7
rootlm <liammillay2@gmail.com>
2018-07-01 13:28:19 -0400

    WHY IS THIS ERRORING

--- chat.cpp
+++ chat.cpp
@@ -20,9 +20,13 @@ chatmsg_t chatmsgs[5];
 chatstate_t chatstate;
 
 void Chat_SetMessage(char* str, unsigned char flags) {
-	memmove(chatmsgs + sizeof(chatmsg_t), chatmsgs, sizeof(chatmsg_t) * 4);
+	//memmove(chatmsgs + sizeof(chatmsg_t), chatmsgs, sizeof(chatmsg_t) * 4);
+	memmove(((char*)chatmsgs) + sizeof(chatmsg_t), chatmsgs, sizeof(chatmsg_t) * 4);
 	//strcpy((char*)&chatmsgs[0].msg,str);
-	chatmsgs[0].msg = str;
+	//chatmsgs[0].msg = str;
+	strcpy(chatmsgs[0].msg, str);
+	chatmsgs[0].flags = flags;
+
 	if (chatstate.msgamount < 4) {
 		chatstate.msgamount++;
 	}
@@ -39,8 +43,33 @@ void Chat_Step() {
 	}
 	chatstate.msg[0] = 65 + (rand() % 27);
 
-	Chat_SetMessage((char*)&chatstate.msg, 0);
-	Sleep(16 * 60);
+	if (rand() % 10 < 5) {
+		strcpy(chatstate.msg, "/me test");
+	}
+	else {
+		strcpy(chatstate.msg, "memes");
+	}
+	//format message
+	unsigned char tempmsgflags = 0;
+
+	char formatmsg[96];
+
+
+	//if (chatstate.msg[0] == 0x2F && chatstate.msg[1] == 0x6D && chatstate.msg[2] == 0x65) { //me check
+	if (memcmp("/me",chatstate.msg,3) == 0) {
+		tempmsgflags |= 2;
+		//copy everything past /me to the start (add player name too but for right now just this)
+		//memcpy(chatstate.msg, &chatstate.msg[4], 61);
+		//add player name without <>
+		strcpy(formatmsg, "PlayerName");
+		//strcat(msgformattemp, chatstate.msg);
+	}
+
+	if (chatstate.msgamount < 4) {
+		Chat_SetMessage(formatmsg, tempmsgflags);
+		//Chat_SetMessage((char*)&formatmsg, tempmsgflags);
+	}
+	//Sleep(16 * 60);
 }
 
 void Chat_Display() {
@@ -59,7 +88,7 @@ void Chat_Display() {
 		unsigned char i = 0;
 		while (i < 5) {
 			if (chatmsgs[i].msg != NULL) {
-				if (chatmsgs[i].flags >= 0) { //if theres any more flags change this to & 1 or & 2 for server/me
+				if (chatmsgs[i].flags > 0) { //if theres any more flags change this to & 1 or & 2 for server/me
 					msgcol = 0xFFFFFF00; //yellow
 				}
 				//font_draw(ctx, cty + (12 * i), chatmsgs[i].msg);
--- chat.h
+++ chat.h
@@ -2,15 +2,15 @@
 #ifndef __CHAT__
 	#define __CHAT__
 
+#define CHATMSGSIZE 64
+
 	typedef struct {
-		char* msg;
+		char msg[CHATMSGSIZE + 17]; //message + <14 char player name> + identifier (host/admin)
 		unsigned char flags;
 		//^:
 		//bit 0=player message or server/event message (EX:xXcritterslayah69Xx has changed name to thelegend27)
 		//bit 1=/me 
 	} chatmsg_t;
-
-#define CHATMSGSIZE 64
 
 	typedef struct {
 		unsigned short timer; //>=1:display chat 0=don't
