73aa860e8f225644753fa21621a9f53b1f19e3ae
rootlm <liammillay2@gmail.com>
2024-08-03 18:52:07 -0400

    Disable logger on MinGW32
    The library used doesn't compile on MinGW32 so stub the logger out if using it

--- src/Utils/Logger.cpp
+++ src/Utils/Logger.cpp
@@ -1,6 +1,11 @@
 #include "Logger.h"
-#include <spdlog/sinks/basic_file_sink.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
+
+//On MinGW32 spdlog will fail to compile due to lacking the c++ thread model, so don't use it on that compiler and stub the function below
+#if !defined(NETX_MINGW32)
+	#include <spdlog/sinks/basic_file_sink.h>
+	#include <spdlog/sinks/stdout_color_sinks.h>
+#endif
+
 #include "../common/misc.h"
 
 #include <iostream>
@@ -13,29 +18,34 @@ namespace Utils
 {
 namespace Logger
 {
-  static const char *LOG_PATTERN = "%^[%H:%M:%S.%e] [%l] [%@ %!]: %v%$";
-  std::vector<spdlog::sink_ptr> sinks;
+	#if defined(NETX_MINGW32)
+		void init(std::string filename) {
 
-  void init(std::string filename)
-  {
-    sinks.push_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-    sinks.push_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>(widen(filename), true));
+		}
+	#else
+		static const char *LOG_PATTERN = "%^[%H:%M:%S.%e] [%l] [%@ %!]: %v%$";
+		std::vector<spdlog::sink_ptr> sinks;
 
-    spdlog::set_default_logger(std::make_shared<spdlog::logger>("nxe logger", begin(sinks), end(sinks)));
+  		void init(std::string filename) {
+			sinks.push_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+			sinks.push_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>(widen(filename), true));
 
-    spdlog::set_pattern(LOG_PATTERN);
+			spdlog::set_default_logger(std::make_shared<spdlog::logger>("nxe logger", begin(sinks), end(sinks)));
 
-    spdlog::set_error_handler([](const std::string &msg) {
-        std::cerr << "spdlog error: " << msg << std::endl;
-    });
+			spdlog::set_pattern(LOG_PATTERN);
 
-    spdlog::flush_on(spdlog::level::debug);
-#if defined(DEBUG)
-    spdlog::set_level(spdlog::level::trace);
-#else
-    spdlog::set_level(spdlog::level::info);
-#endif
-  }
+			spdlog::set_error_handler([](const std::string &msg) {
+			std::cerr << "spdlog error: " << msg << std::endl;
+			});
+
+			spdlog::flush_on(spdlog::level::debug);
+			#if defined(DEBUG)
+				spdlog::set_level(spdlog::level::trace);
+			#else
+				spdlog::set_level(spdlog::level::info);
+			#endif
+		}
+	#endif
 
 
 } // namespace Logger
--- src/Utils/Logger.h
+++ src/Utils/Logger.h
@@ -1,6 +1,9 @@
 #ifndef _LOGGER_H
 
-#include <spdlog/spdlog.h>
+//On MinGW32 spdlog will fail to compile due to lacking the c++ thread model, so don't use it on that compiler and remove the functions using empty defines below
+#if !defined(NETX_MINGW32)
+	#include <spdlog/spdlog.h>
+#endif
 
 #include <string>
 
@@ -14,12 +17,21 @@ namespace Logger
 
   void init(std::string filename);
 
-#define LOG_TRACE SPDLOG_TRACE
-#define LOG_DEBUG SPDLOG_DEBUG
-#define LOG_INFO SPDLOG_INFO
-#define LOG_WARN SPDLOG_WARN
-#define LOG_ERROR SPDLOG_ERROR
-#define LOG_CRITICAL SPDLOG_CRITICAL
+#if defined(NETX_MINGW32)
+	#define LOG_TRACE 
+	#define LOG_DEBUG 
+	#define LOG_INFO 
+	#define LOG_WARN 
+	#define LOG_ERROR 
+	#define LOG_CRITICAL 
+#else
+	#define LOG_TRACE SPDLOG_TRACE
+	#define LOG_DEBUG SPDLOG_DEBUG
+	#define LOG_INFO SPDLOG_INFO
+	#define LOG_WARN SPDLOG_WARN
+	#define LOG_ERROR SPDLOG_ERROR
+	#define LOG_CRITICAL SPDLOG_CRITICAL
+#endif
 
 } // namespace Logger
 } // namespace Utils
